1d07c49ecba09aaa074ac761c1ef0939fe864a3c
Tassia <araujot@vaniercollege.qc.ca>
2025-02-07 11:21:38 -0500

    practice for lab3

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -12,6 +12,20 @@
  * @author Michael Kölling and David J. Barnes
  * @version 2016.02.29
  */
+
+/*
+ * Extra questions:
+ * 
+ * 30) Rectangle window = new Rectangle(5,7);
+ *
+ * 33) p1.print("file1.txt",true);
+ *     p1.print("file2.txt",false);
+ *     
+ *     int status = 0;
+ *     status = p1.getStatus(3);
+ *     status = p1.getStatus(30);
+ */
+
 public class ClockDisplay
 {
     private NumberDisplay hours;
@@ -27,6 +41,8 @@ public class ClockDisplay
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
         updateDisplay();
+              
+        
     }
 
     /**
@@ -36,7 +52,7 @@ public class ClockDisplay
      */
     public ClockDisplay(int hour, int minute)
     {
-        hours = new NumberDisplay(24);
+        hours = new NumberDisplay(12);
         minutes = new NumberDisplay(60);
         setTime(hour, minute);
     }
@@ -51,6 +67,7 @@ public class ClockDisplay
         if(minutes.getValue() == 0) {  // it just rolled over!
             hours.increment();
         }
+        
         updateDisplay();
     }
 
@@ -78,6 +95,10 @@ public class ClockDisplay
      */
     private void updateDisplay()
     {
+        
+        if(hours.getValue()==0){
+           hours.setValue(12);
+        }
         displayString = hours.getDisplayValue() + ":" + 
                         minutes.getDisplayValue();
     }
